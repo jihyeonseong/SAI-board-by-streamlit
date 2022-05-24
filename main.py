cb936cbc691716d5da99207d64cab3bb185fe6b9
jihyeon seong <tjdwltnsfP1@gmail.com>
2022-05-24 23:05:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -154,7 +154,6 @@ def main():
     st.markdown("---")
     ranking = st.sidebar.slider("Select Number of Important Sensor Ranking", 1, 20, value=5)
     rel_rank = pd.read_csv('./data/ranking.csv')
-    rel_rank.set_index('index', inplace=True)
     top_ranking = rel_rank.index[:ranking]
     conf_plot = alt.Chart(top_ranking, title=f"Top {ranking} important sensors", padding={"left": 1, "top": 10, "right": 1, "bottom": 1}
                     ).mark_bar().encode(
